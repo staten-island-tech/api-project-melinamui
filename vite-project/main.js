acbc0f813b1046609351cb22a29388626ffc17e8
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-10 14:01:13 -0500

    pls

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,13 +1,30 @@
 import "./style.css";
 
-const URL = "https://api.quotable.io/random";
+const cat = "https://nekos.best/api/v2/neko";
 
-async function getData(URL) {
+async function getData(cat) {
   try {
-    const response = await fetch(URL);
-    console.log(response);
+    const response = await fetch(cat);
+    if (response.status < 200 || response.statusn > 299) {
+      console.log(response.status);
+      throw error(response);
+    } else {
+      const data = await response.json();
+
+      data.result.forEach((neko) => {
+        document.getElementById("display").insertAdjacentHTML(
+          "afterbegin",
+          ` <div class ="card">
+          <h1>${neko.artist_name}</h1>
+          <img src="${neko.url}" alt ="">
+          </div>`
+        );
+      });
+    }
   } catch (error) {
     console.log(error);
+    console.log("u suck");
+    document.getElementById("display").textContent = "no neko for u";
   }
 }
-getData(URL);
+getData(cat);
