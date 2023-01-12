7f510f057b84a96eb9354475cfb135b390b37fcb
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-12 18:23:41 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -7,10 +7,10 @@ console.log(DOMSelectors);
 const cat = "https://nekos.best/api/v2/neko";
 
 DOMSelectors.display.addEventListener("click", function () {
-  getNeko();  
+  getData(cat);  
 });
 
-let getNeko = async function getData(cat) {
+async function getData(cat) {
   try {
     const response = await fetch(cat);
     if (response.status < 200 || response.status > 299) {
