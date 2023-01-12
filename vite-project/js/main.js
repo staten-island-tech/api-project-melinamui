dcd68b5629b1cf2a1a3da9d0421c7e2b72b54ee6
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-12 17:53:00 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,7 +1,7 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 console.log(DOMSelectors);
-/*
+
 const cat = "https://nekos.best/api/v2/neko";
 //https://cdnb.artstation.com/p/assets/images/images/035/536/863/large/rasel-moe-world3.jpg?1615228697"
 
@@ -30,4 +30,4 @@ async function getData(cat) {
     document.getElementById("display").textContent = "no neko for u";
   }
 }
-getData(cat);*/
+getData(cat);
