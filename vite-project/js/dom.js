3caa211dcdab19e36a58b80494f4cf204e704ed8
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-12 17:42:43 -0500

    testing

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,3 +1,4 @@
 const DOMSelectors = {
     getneko: document.getElementById("getneko"),
 };
+export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,4 +1,6 @@
 import "../styles/style.css";
+import { DOMSelectors } from "./dom";
+console.log(DOMSelectors);
 
 const cat = "https://nekos.best/api/v2/neko";
 //https://cdnb.artstation.com/p/assets/images/images/035/536/863/large/rasel-moe-world3.jpg?1615228697"
