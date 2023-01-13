176a59441726dbad400dbc83182473374538f943
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-12 20:00:42 -0500

    blah

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -6,8 +6,6 @@ console.log(DOMSelectors);
 
 const cat = "https://nekos.best/api/v2/neko";
 
-DOMSelectors.getneko.addEventListener('click', getData(cat))
-
 async function getData(cat) {
   try {
     const response = await fetch(cat);
