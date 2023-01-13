86642f8f9f8cec9099cd753790f3402b66c9b221
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-12 19:34:35 -0500

    testing

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,5 +1,6 @@
 const DOMSelectors = {
     getneko: document.getElementById("getneko"),
     display: document.getElementById("display"),
+    beginning: document.getElementsByClassName("starting-text"),
 };
 export { DOMSelectors };
