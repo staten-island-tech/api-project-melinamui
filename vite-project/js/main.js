2a28d45ea043e11302881fa4e3b657be33c46079
melinamui <113132371+melinamui@users.noreply.github.com>
2023-01-12 19:58:08 -0500

    try

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -6,7 +6,7 @@ console.log(DOMSelectors);
 
 const cat = "https://nekos.best/api/v2/neko";
 
-
+DOMSelectors.getneko.addEventListener("submit", getData(cat))
 
 async function getData(cat) {
   try {
